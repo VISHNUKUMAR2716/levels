0b96a617fddcb4d22f0c7aefdbaa6f97cdda24ed
VISHNUKUMAR2716 <vishnuarun2704@gmail.com>
2025-04-05 11:47:57 +0530

    Add files via upload

--- level-9/src/App.js
+++ level-9/src/App.js
@@ -4,16 +4,36 @@ import User from './Users';
 import Dashboard from './Dashboard';
 import Profile from './Profile';
 import Settings from './Setting';
+import { AuthProvider } from './AuthContext';
 function App() {
   return (
     <div>
-   <Router>
+    <AuthProvider>
+    <Router>
+    <nav>
+    <ul>
+    <li>
+    <Link to="/">Dashboard</Link>
+    </li>
+    <li>
+    <Link to="/users">Users</Link>
+    </li>
+    <li>
+    <Link to="/profile">Profile</Link>
+    </li>
+    <li>
+    <Link to="/settings">Settings</Link>
+    </li>
+    </ul>
+    </nav>
     <Routes>
-      <Route path='/' element={<Dashboard />}/>
-      <Route path="profile" element={<Profile />} />
-      <Route path="settings" element={<Settings />} />
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/users" element={<User />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/settings" element={<Settings />} />
     </Routes>
-   </Router>
+    </Router>
+    </AuthProvider>
     </div>
   
   );
