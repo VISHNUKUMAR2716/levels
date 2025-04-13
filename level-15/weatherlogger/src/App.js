debc7fdd137eea861aa8745375dcf697cd798418
VISHNUKUMAR2716 <vishnuarun2704@gmail.com>
2025-04-13 14:03:54 +0530

    Add files via upload

---
+++ level-15/weatherlogger/src/App.js
@@ -0,0 +1,12 @@
+import WeatherLogger from './WeatherLogger';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <WeatherLogger />
+    </div>
+  );
+}
+
+export default App;
